5294df6578ce6d8b2caeb59f35d91d2c475cd11c
Danilo <danilo.freitas@clubpetro.com>
2022-02-08 22:37:41 -0300

    feat: add tests to signUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,19 @@ describe('Signup', () => {
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    }),
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: "any_password",
+                passwordConfirmation: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'))
     })
 })
