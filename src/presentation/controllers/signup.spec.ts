7ea6ca0691b16ed7e9c09ef8897a389b78eaec6e
Danilo <danilo.freitas@clubpetro.com>
2022-02-09 22:44:25 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -96,4 +96,22 @@ describe('Signup', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
+
+  test('Should call EmailValidator with correct email ', () => {
+    const {sut, emailValidatorStub} = makeSup();
+
+   const isValidSpy = jest.spyOn(emailValidatorStub,'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any',
+        password: 'any_password',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+    
+  });
 });
