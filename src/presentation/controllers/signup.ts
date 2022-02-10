58d2e9a63c0686fac18eefced189ef0b79637fd2
Danilo <danilo.freitas@clubpetro.com>
2022-02-09 23:20:56 -0300

    refactor: move all protocol import single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,5 @@
-import { HttpResponse, HttpRequest } from '../protocols/http';
 import { badRequest, serverError } from '../helpers/http-helpers';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/email-validator';
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols';
 import { InvalidParamError, MissingParamError } from '../errors';
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
