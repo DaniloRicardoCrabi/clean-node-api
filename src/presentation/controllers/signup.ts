ec7e2ba636a176b3b93cdaedcdd048d160b0e2c7
Danilo <danilo.freitas@clubpetro.com>
2022-02-10 22:46:42 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,10 +6,12 @@ import {
   HttpResponse
 } from '../protocols';
 import { InvalidParamError, MissingParamError } from '../errors';
+import { AddAccount } from '../../domain/useCases/add-account';
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly addAccount: AddAccount;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
   }
 
@@ -27,14 +29,23 @@ export class SignUpController implements Controller {
         }
       }
 
-      if(httpRequest.body.password != httpRequest.body.passwordConfirmation) {
+      const {name, email,password,passwordConfirmation} = httpRequest.body
+
+      if(password != passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      const isValid = this.emailValidator.isValid(email);
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
+      
     } catch (error) {
       return serverError();
     }
