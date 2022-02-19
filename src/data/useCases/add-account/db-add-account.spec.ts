233c92e5f678da16a304f6a3658433c814f4135f
Danilo <danilo.freitas@clubpetro.com>
2022-02-18 23:05:30 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -84,4 +84,17 @@ describe('DbAddAccount UseCases', () => {
     });
   });
 
+  test('Should throw if addAccout throws', async () => {
+    const { sut, addAccountRepositoryStub} = makeSut();
+
+    const addSpy = jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve,reject)=> reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow()
+  });
+
 });
